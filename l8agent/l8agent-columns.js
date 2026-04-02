721a90611f642066aef435d81fb19f50a7b9dd02
Sharon Aicler <saichler@gmail.com>
2026-04-02 06:46:52 -0700

    Fix agent bug

--- l8agent/l8agent-columns.js
+++ l8agent/l8agent-columns.js
@@ -15,6 +15,14 @@ Layer 8 Ecosystem is licensed under the Apache License, Version 2.0.
     const render = L8Agent.render;
 
     L8Agent.columns = {
+        L8AgentConversation: [
+            ...col.id('conversationId'),
+            ...col.col('title', 'Title'),
+            ...col.col('userId', 'User'),
+            ...col.status('status', 'Status', enums.CONVO_STATUS_VALUES, render.convoStatus),
+            ...col.date('createdAt', 'Created'),
+            ...col.date('updatedAt', 'Updated')
+        ],
         L8AgentPrompt: [
             ...col.id('promptId'),
             ...col.col('name', 'Name'),
@@ -26,6 +34,7 @@ Layer 8 Ecosystem is licensed under the Apache License, Version 2.0.
     };
 
     L8Agent.primaryKeys = {
+        L8AgentConversation: 'conversationId',
         L8AgentPrompt: 'promptId'
     };
 })();
